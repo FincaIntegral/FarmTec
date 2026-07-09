4590048738c07b39d1fd78d79000e12f18109ab3
Juansantamaria-mon <juan.santamaria2005@gmail.com>
2026-07-08 20:17:01 -0500

    fix en cors

--- ganadosmart/backend/src/main.ts
+++ ganadosmart/backend/src/main.ts
@@ -7,7 +7,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
-  app.enableCors();
+  app.enableCors({
+      origin: [
+        'https://farm-tec.vercel.app',
+        'http://localhost:4200',
+      ],
+      methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+      credentials: true,
+    });
 
   app.useGlobalPipes(
     new ValidationPipe({
